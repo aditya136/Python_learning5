b69ef9a8c4662837ea452fb06ddca6c8065ba35c
aditya136 <adityabanerjee102@gmail.com>
2022-10-15 09:17:55 -0700

    Python_AI

---
+++ pythonspekar.py
@@ -0,0 +1,6 @@
+import pyttsx3
+
+tts = tts = pyttsx3.init()
+
+tts.say('Hello world, I am a robot from python AI. Aditya banerjee made me. I am a interligent robot who can do anythings.')
+tts.runAndWait()
